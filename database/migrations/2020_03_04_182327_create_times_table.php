5b0ca5cfb1506c88ba4b387cb1c242ec09a8ea8e
reszi <gabor.reszler413@gmail.com>
2020-03-26 19:01:42 +0100

    create Time model and its migration

---
+++ database/migrations/2020_03_04_182327_create_times_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('times', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->time('value');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('times');
+    }
+}
