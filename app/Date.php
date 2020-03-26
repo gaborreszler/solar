de86c6ad9dc0174476bfe446beb25ac47e4f8ab2
reszi <gabor.reszler413@gmail.com>
2020-03-26 18:51:45 +0100

    create Date model and its migration

---
+++ app/Date.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Date extends Model
+{
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['value'];
+
+	/**
+	 * Indicates if the model should be timestamped.
+	 *
+	 * @var bool
+	 */
+	public $timestamps = false;
+}
